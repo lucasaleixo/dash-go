f6155724881c7adc6e4d24288ccda1544dd146da
lucasaleixo <lap15.ufpr@gmail.com>
2022-03-15 19:38:02 -0300

    added responsive breakpoints to the Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lucas Aleixo</Text>
-        <Text color="gray.300" fontSize="small">
-          lap15.ufpr@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Lucas Aleixo</Text>
+          <Text color="gray.300" fontSize="small">
+             lap15.ufpr@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Lucas Aleixo" src="https://github.com/lucasaleixo.png" />
     </Flex>
