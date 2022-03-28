dd59258c37ba0b4639b2eec696f821b257dd6595
lucasaleixo <lap15.ufpr@gmail.com>
2022-03-28 11:57:20 -0300

    finished pagination

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { useMutation } from 'react-query';
 
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -8,6 +9,9 @@ import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { appendErrors, SubmitHandler, useForm } from "react-hook-form";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -26,14 +30,31 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation( async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   });
 
   const handleCreateUser:SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    
-    console.log(values);
+    await createUser.mutateAsync(values);
+
+    router.push('/users');
   }
 
   const { errors } = formState;
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs'
+import { createServer, Factory, Model, Response, ActiveModelSerializer } from 'miragejs'
 import { faker } from '@faker-js/faker'
 
 type User = {
@@ -10,6 +10,10 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+    
     models: {
       user: Model.extend<Partial<User>>({})
     },
@@ -45,7 +49,9 @@ export function makeServer() {
         const pageEnd = pageStart + Number(per_page);
 
         const users = this.serialize(schema.all('user'))
-          .users.slice(pageStart, pageEnd);
+          .users
+          .sort((a,b) => a.createdAt - b.createdAt)
+          .slice(pageStart, pageEnd);
 
         return new Response(
           200, 
@@ -54,6 +60,7 @@ export function makeServer() {
         )
       });
 
+      this.get('/users/:id');
       this.post('/users');
 
       this.namespace = '';
